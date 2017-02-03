617c57e176cfd6add4902e95511f5af0743e65f6
Rick Swan <thisisricksemail@gmail.com>
2017-02-02 21:20:50 -0600

    Fixed comment

--- Collatz.py
+++ Collatz.py
@@ -17,7 +17,8 @@ globmemotab = {}
 # collatz_read
 # ------------
 
-def collatz_read (r) :
+
+def collatz_read(r):
     """
     read an int from r
     r a reader
@@ -31,7 +32,8 @@ def collatz_read (r) :
 # collatz_length
 # --------------
 
-def collatz_length (r) :
+
+def collatz_length(r):
     """
     calculates cycle length for a number
     r a number
@@ -46,9 +48,9 @@ def collatz_length (r) :
     if r == 1:
         return 1
     if r % 2 == 0:
-        globmemotab[r] = 1 + collatz_length(r/2)
+        globmemotab[r] = 1 + collatz_length(r / 2)
         return globmemotab[r]
-    globmemotab[r] = 1 + collatz_length(r*3 +1)
+    globmemotab[r] = 1 + collatz_length(r * 3 + 1)
     assert globmemotab[r] > 0
     return globmemotab[r]
 
@@ -56,26 +58,27 @@ def collatz_length (r) :
 # collatz_eval
 # ------------
 
-def collatz_eval (n) :
+
+def collatz_eval(n):
     """
     n the end of the range [1, n], inclusive
     return the max cycle length of the range [1, n]
     """
     # <your code>
 
-    if n < 5000000 :
-        metacache = [1, 2, 3, 6, 7, 9, 18, 19, 25, 27, 54, 55, 73, 97, 129, 171, 231, 235, 313, 327, 649, 654, 655, 667, 703, 871, 1161, 2223, 2322, 2323, 2463, 2919, 3711, 6171, 10971, 13255, 17647, 17673, 23529, 26623, 34239, 35497, 35655, 52527, 77031, 106239, 142587, 156159, 216367, 230631, 410011, 511935, 626331, 837799, 1117065, 1126015, 1501353, 1564063, 1723519, 2298025, 3064033, 3542887, 3732423]
-        for i in range(len(metacache)) :
-            if metacache[i] > n :
-                return metacache[i-1]
+    if n < 5000000:
+        metacache = [1, 2, 3, 6, 7, 9, 18, 19, 25, 27, 54, 55, 73, 97, 129, 171, 231, 235, 313, 327, 649, 654, 655, 667, 703, 871, 1161, 2223, 2322, 2323, 2463, 2919, 3711, 6171, 10971, 13255, 17647, 17673,
+                     23529, 26623, 34239, 35497, 35655, 52527, 77031, 106239, 142587, 156159, 216367, 230631, 410011, 511935, 626331, 837799, 1117065, 1126015, 1501353, 1564063, 1723519, 2298025, 3064033, 3542887, 3732423]
+        for i in range(len(metacache)):
+            if metacache[i] > n:
+                return metacache[i - 1]
         return metacache[-1]
-
 
     if n == 1:
         return 1
     q = 1
     p = 1
-    for i in range(n//2, n):
+    for i in range(n // 2, n):
         if collatz_length(i) >= q:
             q = collatz_length(i)
             p = i
@@ -86,10 +89,10 @@ def collatz_eval (n) :
 # collatz_meta
 # ------------
 
-def collatz_meta (n) :
+def collatz_meta(n):
     """
-    n the end of the range [1, n], inclusive
-    return the max cycle length of the range [1, n]
+    I used this to make meta cache
+    pretty cool huh?
     """
     q = 1
     p = 1
@@ -97,13 +100,14 @@ def collatz_meta (n) :
         if collatz_length(i) >= q:
             q = collatz_length(i)
             p = i
-            print( str(p))
+            print(str(p))
 
 # -------------
 # collatz_print
 # -------------
 
-def collatz_print (w, m) :
+
+def collatz_print(w, m):
     """
     print an int to w
     w a writer
@@ -116,13 +120,14 @@ def collatz_print (w, m) :
 # collatz_solve
 # -------------
 
-def collatz_solve (r, w) :
+
+def collatz_solve(r, w):
     """
     r a reader
     w a writer
     """
     t = int(r.readline())
-    for _ in range(t) :
+    for _ in range(t):
         n = collatz_read(r)
         m = collatz_eval(n)
         collatz_print(w, m)
